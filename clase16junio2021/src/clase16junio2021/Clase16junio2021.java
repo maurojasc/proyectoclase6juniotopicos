987a023da63d6de0cb71b408aff1999483b76c2e
Mauricio <Mauricio@DESKTOP-H31JITJ>
2021-06-16 08:35:36 -0500

    commit 0 Mauricio

---
+++ clase16junio2021/src/clase16junio2021/Clase16junio2021.java
@@ -0,0 +1,15 @@
+
+package clase16junio2021;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Clase16junio2021 {
+
+
+    public static void main(String[] args) {
+        System.out.println("versión 0 Tags");
+    }
+    
+}
